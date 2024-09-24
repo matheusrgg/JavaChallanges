5a81a68154087325e345f4e1cf7b111108b6c612
matheusrgg <matheusrgg@gmail.com>
2024-09-24 14:11:56 -0300

    Exercicios de Collection

--- src/Collection/ListaTarefas.java
+++ src/Collection/ListaTarefas.java
@@ -1,7 +1,15 @@
 package Collection;
+import java.util.ArrayList;
 
 public class ListaTarefas {
     
-    
-
+    public class Tarefa {
+        String descrição ; 
+    }
+   
+  
+    public void adicionarTarefa(String descricao){
+        ArrayList<Tarefa> listaTarefas = new ArrayList<Tarefa>();
+        listaTarefas.add(1,"Exemplo Tarefa 1");
+    }
 }
---
+++ src/CollectionExercicios/ListaTarefa.java
@@ -0,0 +1,16 @@
+package CollectionExercicios;
+
+import java.util.List;
+import java.util.ArrayList;
+
+
+
+public class ListaTarefa {
+    
+    //atributo
+    private List<Tarefa> tarefaLista;
+
+    public ListaTarefa(){
+        this.tarefaLista = new ArrayList<>();
+    }
+}
